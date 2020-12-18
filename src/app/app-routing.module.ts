259b16c20f3a7e6b1737ce219783926b9dfaa2a5
NevinJulian <nevinhelfenstein@hotmail.ch>
2020-12-18 07:57:56 +0100

    center player info and add manual page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { GameComponent } from './game/game.component';
+import { ManualComponent } from './manual/manual.component';
 
 const routes: Routes = [
     { path: '', component: GameComponent },
+    { path: 'manual', component: ManualComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GameComponent } from './game/game.component';
+import { ManualComponent } from './manual/manual.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GameComponent
+    GameComponent,
+    ManualComponent
   ],
   imports: [
     BrowserModule,
