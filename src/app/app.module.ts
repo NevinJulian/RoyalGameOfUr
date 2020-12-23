65b06c205fb292351edd282d5dffc322e4073a9e
NevinJulian <nevinhelfenstein@hotmail.ch>
2020-12-23 16:22:59 +0100

    add about page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GameComponent } from './game/game.component';
 import { ManualComponent } from './manual/manual.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GameComponent,
-    ManualComponent
+    ManualComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
