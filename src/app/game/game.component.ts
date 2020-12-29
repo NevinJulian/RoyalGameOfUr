eb3778b10e9062d98813db87ab5099fddc35d4b8
NevinJulian <nevinhelfenstein@hotmail.ch>
2020-12-29 14:35:18 +0100

    add names for item divs

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { generateStartingGameState, throwDice } from 'src/model/src/Game';
 import { GameState } from 'src/model/src/GameState';
 
@@ -24,5 +24,17 @@ export class GameComponent implements OnInit {
 
   throwDice() {
     this.diceThrow = throwDice();
+  }
+
+  placeStone() {
+    document.getElementsByName("1")[0].innerHTML += "<img class=\"stones\" src=\"../assets/img/black-stone.svg\">"
+  }
+
+  renderGameState() {
+    for (let i = 0; i <= this.gameState.board.length; i++) {
+      if (this.gameState.board[i].stone) {
+        document.getElementsByName(i.toString())[0].innerHTML = "<img class=\"images\" src=\"../assets/img/black-stone.svg\" style=\"position: absolute;\">"
+      }
+    }
   }
 }
